bc10a944207927f5d83c27d1792d9e76354b1396
Gabriel Lucas <lucas@byinti.com>
2022-01-02 23:02:26 -0300

    Api setup

---
+++ apps/api/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  displayName: 'api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/api',
+  coverageReporters: [
+    'html',
+    'text-summary'
+  ],
+  rootDir: ".",
+  collectCoverage: true,
+
+};
